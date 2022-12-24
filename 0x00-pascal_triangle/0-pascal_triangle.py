f7b7ca460b6fa05bc0de76ec0aec3b544d06ab64
ambroseotundo <ambroseotundo9@gmail.com>
2022-12-24 05:44:12 -0500

    rewrite the script

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,25 +1,40 @@
 #!/usr/bin/python3
-def pascal_triangle(n):
-    triangle = []
+"""
+Defines function that returns a list of lists of integers
+representing the Pascal's triangle of n
+"""
 
-    # Return an empty list if n is not positive
+
+def pascal_triangle(n):
+    """
+    Creates a list of lists of integers representing Pascal's triangle
+
+    parameters:
+        n [int]:
+            the number of rows of Pascal's triangle to recreate
+
+    return:
+        [list of lists of ints]:
+            representation of Pascal's triangle
+    """
+    if type(n) is not int:
+        raise TypeError("n must be an integer")
+    triangle = []
     if n <= 0:
         return triangle
-
-    # Generate each row of the triangle
-    for i in range(n):
-        # The first and last element of each row is always 1
-        row = [1]
-
-        # If this is not the first row, generate the elements of the row
-        if i > 0:
-            # Generate the elements of the row by adding the adjacent elements of the previous row
-            for j in range(i - 1):
-                row.append(triangle[i - 1][j] + triangle[i - 1][j + 1])
-            # The last element of the row is also 1
-            row.append(1)
-
-        # Add the row to the triangle
-        triangle.append(row)
-
+    previous = [1]
+    for row_index in range(n):
+        rowlist = []
+        if row_index == 0:
+            rowlist = [1]
+        else:
+            for i in range(row_index + 1):
+                if i == 0:
+                    rowlist.append(0 + previous[i])
+                elif i == (row_index):
+                    rowlist.append(previous[i - 1] + 0)
+                else:
+                    rowlist.append(previous[i - 1] + previous[i])
+        previous = rowlist
+        triangle.append(rowlist)
     return triangle
